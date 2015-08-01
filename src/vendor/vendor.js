79e4edace55b510d0d37513eba9bcc019e5e2692
xenio <xeniotech@gmail.com>
2015-07-31 19:18:13 -0500

    added bootstrap modal

--- src/vendor/vendor.js
+++ src/vendor/vendor.js
@@ -1,6 +1,7 @@
 import './styles';
 import 'jquery/dist/jquery.js';
 import 'bootstrap/js/dropdown.js';
+import 'bootstrap/js/modal.js';
 import './angular';
 import './international-phone.js';
 import 'satellizer/satellizer.js';
